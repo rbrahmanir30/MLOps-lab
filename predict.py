76c7676ed008efd10477e6b103a2c8e046d279d9
rbrahmanir30 <r.brahmani.r30@gmail.com>
2026-09-10 17:05:05 +0530

    Update by Brahmani

--- predict.py
+++ predict.py
@@ -3,10 +3,10 @@ import joblib
 model = joblib.load("billing_model.pkl")
 
 # Example Vodafone customer usage
-data_gb = 12
-voice_minutes = 350
-sms_count = 180
-international_minutes = 15
+data_gb = 14
+voice_minutes = 500
+sms_count = 200
+international_minutes = 20
 
 prediction = model.predict([[
     data_gb,
